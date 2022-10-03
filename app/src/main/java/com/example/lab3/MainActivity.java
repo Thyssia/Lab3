c6d316926814f854a8e595062cbf3080c67e81fc
Thyssia Reid <soar0018@algonquinlive.com>
2022-10-03 13:32:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.lab3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button next;
+    EditText name;
+    SharedPreferences sharedpreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.textName);
+        next = (Button) findViewById(R.id.button);
+
+
+
+        next.setOnClickListener(click -> {
+            Intent myIntent = new Intent(MainActivity.this, NameActivity.class);
+            myIntent.putExtra("name", name.getText().toString());
+
+            startActivityForResult(myIntent, 2);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sharedpreferences = getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+        String user = sharedpreferences.getString("name", null);
+        if (user != null) {
+            SharedPreferences.Editor myEdit = sharedpreferences.edit();
+            myEdit.putString("name", name.getText().toString());
+            myEdit.apply();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            if (resultCode == RESULT_OK) {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String s1 = sh.getString("name", "");
+        name.setText(s1);
+    }
+}
